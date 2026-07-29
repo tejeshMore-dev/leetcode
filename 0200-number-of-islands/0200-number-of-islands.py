7c9f4be610daa30b5af0d425762c5e734263c8ae
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-29 15:21:05 +0530

    Time: 249 ms (44.19%), Space: 21.4 MB (91.32%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        DIRECTIONS = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        ans = 0
+
+        def mark_island_visted(r, c): # O(mn), O(1)
+            queue = deque()
+            queue.append([r, c])
+
+            while queue:
+                r, c = queue.popleft()
+
+                for dr, dc in DIRECTIONS:
+                    nr = dr + r
+                    nc = dc + c
+
+                    if nr < 0 or nc < 0 or nr == ROWS or nc == COLS or grid[nr][nc] != "1":
+                        continue
+                    
+                    grid[nr][nc] = "#"
+                    queue.append([nr, nc])
+
+        # count and mark island
+        for  r in range(ROWS): # O(mn), O(1) + # O(mn), O(1)
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    ans += 1
+                    mark_island_visted(r, c) 
+
+
+        #unmark island
+        for  r in range(ROWS): # O(mn), O(1)
+            for c in range(COLS):
+                if grid[r][c] == "#":
+                    grid[r][c] = "1"
+            
+        return ans
+
+
+        
