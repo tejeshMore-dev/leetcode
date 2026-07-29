ed0a29681c342482f1d6a4507d5b04d52977427b
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-29 18:27:38 +0530

    Time: 19 ms (19.66%), Space: 23.9 MB (16.67%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+
+    @staticmethod
+    def _get_hash_key(word: str) -> tuple:
+        key = [0] * 26
+
+        for char in word:
+            key[ ord(char) - ord('a') ] += 1
+        
+        return tuple(key)
+
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+            TC : O(n * k)
+            SC : O(k)
+        '''
+        groups = defaultdict(list)
+        
+        for word in strs:
+            key = self._get_hash_key(word)
+
+            groups[key].append(word)
+        
+
+        return list(groups.values())
+
+
+
+        
