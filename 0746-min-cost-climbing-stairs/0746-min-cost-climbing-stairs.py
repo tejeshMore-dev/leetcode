12d73cabccb3d5281d1537224ac8c8c02f23ddd5
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-30 12:04:42 +0530

    Time: 2 ms (66.99%), Space: 19.5 MB (24.93%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,18 +1,31 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        mem = {}
-
-        def helper(i):
-            if i in mem:
-                return mem[i]
-
-            if i >= len(cost):
-                return  0
-            
-            ans = cost[i] + min(helper(i+1), helper(i+2))
-            mem[i] = ans
-
-            return ans
+        if len(cost) < 3:
+            return min(cost)
         
-        helper(0)
+        l = len(cost)
+        mem = cost.copy()
+        
+        for i in range(l-3, -1, -1):
+            mem[i] = cost[i] + min(mem[i+1], mem[i+2])
+        
         return min(mem[0], mem[1])
+
+        ## recursion approach
+        
+        # mem = {}
+
+        # def helper(i):
+        #     if i in mem:
+        #         return mem[i]
+
+        #     if i >= len(cost):
+        #         return  0
+            
+        #     ans = cost[i] + min(helper(i+1), helper(i+2))
+        #     mem[i] = ans
+
+        #     return ans
+        
+        # helper(0)
+        # return min(mem[0], mem[1])
