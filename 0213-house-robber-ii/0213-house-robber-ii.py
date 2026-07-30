a5aaa0d3c35a15b37311763360b761631627c972
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-30 13:16:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.67%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,31 +1,52 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        '''
+        TC : O(n)
+        SC : O(1)
+        '''
+
         l = len(nums)
         if l < 3:
             return max(nums)
+
+        def helper(s, e):
+            two = nums[e]
+            one = max(two, nums[e-1])
+
+            for i in range(e-2, s-1, -1):
+                one, two = max(nums[i] + two, one), one
             
-        mem = {} # maximum we can starting i
+            return one
 
-        def helper(i, start):
-            if (i, start) in mem:
-                return mem[(i, start)]
 
-            if i == l - 1 and not start:
-                return nums[i]
+        return max(helper(0, l-2), helper(1, l-1))
 
-            if i >= l - 1:                
-                return 0
+        # l = len(nums)
+        # if l < 3:
+        #     return max(nums)
 
-            # rob 
-            rob = nums[i] + helper(i + 2, start)
+        # mem = {} # maximum we can starting i
 
-            # skip
-            skip = helper(i + 1, start)
+        # def helper(i, start):
+        #     if (i, start) in mem:
+        #         return mem[(i, start)]
 
-            ans = max(rob, skip)
-            mem[(i, start)] = ans
-            return ans
+        #     if i == l - 1 and not start:
+        #         return nums[i]
 
-        return max(helper(0, True), helper(1, False))
+        #     if i >= l - 1:                
+        #         return 0
+
+        #     # rob 
+        #     rob = nums[i] + helper(i + 2, start)
+
+        #     # skip
+        #     skip = helper(i + 1, start)
+
+        #     ans = max(rob, skip)
+        #     mem[(i, start)] = ans
+        #     return ans
+
+        # return max(helper(0, True), helper(1, False))
 
         
