423db5458ce880150b9714008f6e52da88d2bb4e
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-03 00:46:47 +0530

    Time: 11 ms (31.58%), Space: 19.7 MB (38.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,10 +5,10 @@ class Solution:
         r = n - 1
 
         while l <= r:
-            while l <= r and not s[l].isalnum():
+            while l < r and not s[l].isalnum():
                 l += 1
 
-            while l <= r and not s[r].isalnum():
+            while l < r and not s[r].isalnum():
                 r -= 1
             
             if l > r:
