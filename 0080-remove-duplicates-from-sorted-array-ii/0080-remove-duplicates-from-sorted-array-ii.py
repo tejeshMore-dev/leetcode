5cce6592fbc499eaa038578fba887c1570d0f1e6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-03 16:05:17 +0530

    Time: 84 ms (73.11%), Space: 21.8 MB (93.22%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,17 +4,12 @@ class Solution:
         if n < 3:
             return n
 
-        second_prev = nums[0]
-        prev = nums[1]
         write_index = 2
 
         for i in range(2, n):
-            if not ((nums[i] == prev) and (prev == second_prev)):
+            if nums[i] != nums[write_index - 2]:
                 nums[write_index] = nums[i]
                 write_index += 1
-            
-            second_prev = prev
-            prev = nums[i]
         
         return write_index
         
