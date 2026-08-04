44e2718500dd3e0c6d2e7532a3de9c4973fe3274
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-04 16:54:10 +0530

    Time: 2 ms (36.81%), Space: 19.4 MB (43.67%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -22,9 +22,14 @@ class Solution:
 
             elif nums1[i1] < nums2[i2]:
                 i1 += 1
+                
+                while i1 < n1 and nums1[i1] == nums1[i1 - 1]:
+                    i1 += 1
+
             else:
                 i2 += 1
-            
 
-            
+                while i2 < n2 and nums2[i2] == nums2[i2 - 1]:
+                    i2 += 1
+
         return ans
