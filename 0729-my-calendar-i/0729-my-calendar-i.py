c1aac04e29ba43c52ca78a3fdf36b1ee0d5d8d28
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-05 23:01:42 +0530

    Time: 1426 ms (5.06%), Space: 20.2 MB (57.13%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,35 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.events = []
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        if not self.events:
+            self.events.append((startTime, 1))
+            self.events.append((endTime, -1))
+            return True
+        
+        bookings = 0
+        previous = self.events[0][0]
+
+        for position, change in self.events:
+            overlap =  max(previous, startTime) < min(position, endTime)
+
+            if overlap and bookings == 1:
+                return False
+            
+            bookings += change
+            previous = position
+        
+        self.events.append((startTime, 1))
+        self.events.append((endTime, -1))
+        self.events.sort()
+        return True
+
+        
+
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
