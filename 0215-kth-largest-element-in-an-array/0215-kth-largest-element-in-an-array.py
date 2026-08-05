d7939c346646460c16a73d2fb0b42ae7dffa64b7
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-05 20:12:57 +0530

    Time: 107 ms (32.03%), Space: 31.1 MB (47.93%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,5 @@
 import heapq
+import random
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
@@ -8,9 +9,34 @@ class Solution:
         SC: O(k)
 
         '''
+
+
+        # n = len(nums)
+        # l, r = 0, n - 1
+        # target = n - k
+        
+        # while l <= r:
+        #     pivot = random.randint(l, r)
+        #     nums[r], nums[pivot] = nums[pivot], nums[r]
+            
+        #     position = l
+        #     for i in range(l, r):
+        #         if nums[i] <= nums[r]:
+        #             nums[position], nums[i] = nums[i], nums[position]
+        #             position += 1
+            
+        #     nums[position], nums[r] = nums[r], nums[position]
+
+        #     if position == target:
+        #         return nums[position]
+        #     elif position < target:
+        #         l = position + 1
+        #     else:
+        #         r = position - 1
+
         min_heap = []
 
-        for num in nums: # TC:O(n log n), SC:O(n)
+        for num in nums: # TC:O(n log k), SC:O(n)
             heapq.heappush(min_heap, num)
 
             if len(min_heap) > k:
