d9941a6d042e83f53f7d4e97a1ab0f20b772d76d
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-12 18:54:13 +0530

    Time: 3 ms (24.52%), Space: 20 MB (12.36%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -6,13 +6,22 @@ class Solution:
         ROWS = len(mat)
         COLS = len(mat[0])
 
+        def count_soldiers(row: int) -> int:
+            l = 0
+            r = COLS
+
+            while l < r:
+                mid = l + (r - l) // 2
+                if mat[row][mid] == 0:
+                    r = mid
+                else: 
+                    l = mid + 1
+            
+            return l
+
         for r in range(ROWS):
             soldiers = 0
-    
-            for c in range(COLS):
-                if mat[r][c] == 1:
-                    soldiers += 1
-            
+            soldiers = count_soldiers(r)
             heapq.heappush(max_heap, (-soldiers, -r))
 
             if len(max_heap) > k:
