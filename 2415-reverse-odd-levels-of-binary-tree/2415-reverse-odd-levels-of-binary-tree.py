2a57d5febcd8eb7971cea91826466bea29731260
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-13 16:25:04 +0530

    Time: 12 ms (91.81%), Space: 23.5 MB (83.15%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -8,28 +8,44 @@ class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return root
-
-        queue = deque([root])
-        level = 0
-
-        while queue:
-            queue_length = len(queue)
-
-            num = [node.val for node in queue]
-
-            for i in range(queue_length):
-                node = queue.popleft()
-
-                if level % 2 != 0:
-                    node.val = num[-1 - i]
-
-                if node.left:
-                    queue.append(node.left)
-                
-                if node.right:
-                    queue.append(node.right)
+        
+        def helper(left, right, level):
+            if not left:
+                return
             
-            level += 1
+            if level % 2 != 0:
+                left.val, right.val = right.val, left.val
 
-            
+            helper(left.left, right.right, level + 1)
+            helper(left.right, right.left, level + 1)
+
+        helper(root.left, root.right, 1)
         return root
+
+        # if not root:
+        #     return root
+
+        # queue = deque([root])
+        # level = 0
+
+        # while queue:
+        #     queue_length = len(queue)
+
+        #     num = [node.val for node in queue]
+
+        #     for i in range(queue_length):
+        #         node = queue.popleft()
+
+        #         if level % 2 != 0:
+        #             node.val = num[-1 - i]
+
+        #         if node.left:
+        #             queue.append(node.left)
+                
+        #         if node.right:
+        #             queue.append(node.right)
+            
+        #     level += 1
+
+            
+        # return root
