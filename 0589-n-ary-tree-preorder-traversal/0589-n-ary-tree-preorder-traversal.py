c64a0657c38eadd4cb75990e6fda6a5fde12d3db
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-13 12:07:06 +0530

    Time: 40 ms (98.64%), Space: 21.1 MB (22.42%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        ans = []
+
+        def helper(node):
+            if not node:
+                return 
+            
+            ans.append(node.val)
+
+            for child in node.children:
+                helper(child)
+        
+        helper(root)
+        return ans
