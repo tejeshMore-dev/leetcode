eaac4b0e936ebd5d497bb835609a2c97096a1cc5
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-14 17:33:02 +0530

    Time: 4 ms (95.77%), Space: 22.5 MB (84.37%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,15 +1,13 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        graph1 = defaultdict(list)
-        graph2 = defaultdict(list)
+        score = [0] * (n + 1)
 
         for a, b in trust:
-            graph1[b].append(a)
-            graph2[a].append(b)
-
+            score[b] += 1
+            score[a] -= 1
         
         for person in range(1, n + 1):
-            if len(graph1[person]) == n - 1 and  len(graph2[person]) == 0 :
+            if score[person] == n - 1:
                 return person
         
         return -1
