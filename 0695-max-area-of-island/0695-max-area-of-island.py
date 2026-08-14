11f103ae3a0c79a327b9e1747f271ed1b6d53109
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-14 19:42:23 +0530

    Time: 15 ms (84.51%), Space: 19.3 MB (95.63%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        max_area = 0
+        DIRECTIONS = [ [1, 0], [-1, 0], [0, 1], [0, -1] ]
+
+        def calculate_area(r, c):
+            stack = [(r, c)]
+            ans = 0
+            grid[r][c] = -1
+            
+            while stack:
+                r, c = stack.pop()
+                ans += 1
+
+                for dr, dc in DIRECTIONS:
+                    nr = dr + r
+                    nc = dc + c
+
+                    if (
+                        0 <= nr < ROWS
+                        and 0 <= nc < COLS
+                        and grid[nr][nc] == 1
+                    ):
+                        grid[nr][nc] = -1
+                        stack.append((nr, nc))
+            
+            return ans
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = calculate_area(r, c)
+                    max_area = max(max_area, area)
+        
+        return max_area
