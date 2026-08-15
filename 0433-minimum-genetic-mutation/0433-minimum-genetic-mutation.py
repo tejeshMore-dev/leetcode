ab3edd6fbfc9f0bde4dd55c46e2e270a1ab570b7
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-15 11:52:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (40.04%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -3,8 +3,6 @@ class Solution:
         bank_set = set(bank)
         queue = deque([ (startGene, 0) ])
         CHOICES = [ "A", "C", "G", "T" ]
-        visited = set()
-        visited.add(startGene)
 
         while queue:
             string, mutations = queue.popleft()
@@ -19,8 +17,8 @@ class Solution:
                         result[i] = choice
                         result_str = "".join(result)
 
-                        if result_str in bank_set and result_str not in visited:
-                            visited.add(result_str)
+                        if result_str in bank_set:
+                            bank_set.remove(result_str)
                             queue.append((result_str, mutations + 1 ))
         
         return -1
