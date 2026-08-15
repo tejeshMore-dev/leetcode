ccf21a8366f0b01e028798b913b0ac1b2029d991
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-15 12:13:18 +0530

    Time: 362 ms (40.64%), Space: 20.5 MB (25.85%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,31 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        start = "0000"
+        deadends_set = set(deadends)
+
+        if start in deadends_set:
+            return -1
+
+        queue = deque([ (start, 0) ])
+        visited = set()
+        visited.add(start)
+        
+        while queue:
+            current, moves = queue.popleft()
+
+            if current == target:
+                return moves
+            
+            for i, slot in enumerate(current):
+                new_up = current[:i] + str((int(slot) + 1) % 10) + current[i+1:]
+                new_down = current[:i] + str((int(slot) - 1) % 10) + current[i+1:]
+
+                if new_up not in deadends_set and new_up not in visited:
+                    visited.add(new_up)
+                    queue.append((new_up, moves + 1))
+
+                if new_down not in deadends_set and new_down not in visited:
+                    visited.add(new_down)
+                    queue.append((new_down, moves + 1))
+
+        return -1
