51f4e15c0d1136f5645eba252fdfe32ecd1c92fc
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-16 15:24:52 +0530

    Time: 4 ms (59.46%), Space: 19.9 MB (23.98%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,17 +1,21 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        ans = set()
+        ans = []
         path = []
         used = [False] * n
         nums.sort()
 
         def backtrack():
             if len(path) == n:
-                ans.add(tuple(path))
+                ans.append(path.copy())
             
+
             for i in range(n):
                 if used[i]:
+                    continue
+                
+                if i > 0 and nums[i] == nums[i -1] and not used[i - 1]:
                     continue
                 
                 path.append(nums[i])
@@ -23,5 +27,5 @@ class Solution:
                 used[i] = False
 
         backtrack()
-        return list(ans) 
+        return ans 
         
