406a78436356f28371dcb8ad52730fea5e4f428b
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-18 14:26:49 +0530

    Time: 363 ms (49.39%), Space: 44.4 MB (58.73%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -4,7 +4,6 @@ class Solution:
         n = len(text2)
 
         dp = [ [0] * (n+1) for _ in range(m+1) ]
-        ans = 0
         
         for i in range(1, m+1):
             for j in range(1, n+1):
@@ -12,8 +11,5 @@ class Solution:
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-                
-                ans = max(ans, dp[i][j])
-
         
-        return ans
+        return dp[-1][-1]
