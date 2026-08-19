11632df06ce9f177d21c9c96bd3fcbc1ee4e504a
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-08-19 15:01:35 +0530

    Time: 989 ms (7.91%), Space: 19.6 MB (76.52%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        dp = [ 
+            [False] * numCourses
+            for _ in range(numCourses)
+        ]
+
+        for v in range(numCourses):
+            dp[v][v] = True
+        
+        for u, v in prerequisites:
+            dp[u][v] = True
+
+        for k in range(numCourses):
+            for u in range(numCourses):
+                for v in range(numCourses):
+                    dp[u][v] = dp[u][v] or ( dp[u][k] and dp[k][v] )
+        
+        ans = []
+        for u, v in queries:
+            ans.append(dp[u][v])
+        
+        return ans
