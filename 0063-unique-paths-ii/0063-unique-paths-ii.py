8bde8b8244d1f74638a6d2fdc560cebf7e80f387
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-02 15:11:27 +0530

    Time: 2 ms (25.13%), Space: 19.3 MB (62.08%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -6,26 +6,24 @@ class Solution:
         if obstacleGrid[0][0] == 1 or obstacleGrid[ROWS-1][COLS-1] == 1:
             return 0
             
-        dp = [ [0] * COLS for _ in range(ROWS) ]
+        dp = [0] * COLS
         
         for c in range(COLS):
             if obstacleGrid[0][c] == 0:
-                dp[0][c] = 1
-            else:
-                break
-        
-        for r in range(ROWS):
-            if obstacleGrid[r][0] == 0:
-                dp[r][0] = 1
+                dp[c] = 1
             else:
                 break
         
         for r in range(1, ROWS):
             for c in range(1, COLS):
+                # First column
+                if obstacleGrid[r][0] == 1:
+                    dp[0] = 0
+
                 if obstacleGrid[r][c]:
-                    dp[r][c] = 0
+                    dp[c] = 0
                     continue
 
-                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+                dp[c] = dp[c] + dp[c-1]
         
-        return dp[ROWS-1][COLS-1]
+        return dp[-1]
