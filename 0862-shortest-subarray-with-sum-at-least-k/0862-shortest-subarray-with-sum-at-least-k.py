58814412d57f99861a0d69598fcd96e6b0803ee9
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-22 15:13:22 +0530

    Time: 127 ms (27.54%), Space: 26.1 MB (83.31%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,27 +1,25 @@
 class Solution:
-    def shortestSubarray(self, nums: List[int], k: int) -> int:
+    def shortestSubarray(self, nums: list[int], k: int) -> int:
+        INF = float('INF')
         N = len(nums)
-        INF = float('inf')
 
         prefix_sum = [0] * (N + 1)
         for i, num in enumerate(nums):
-            prefix_sum[i+1] = num + prefix_sum[i]
-
-        queue = deque([])
+            prefix_sum[i+1] = prefix_sum[i] + num
+        
+        queue = deque()
         ans = INF
 
         for i in range(N + 1):
             while queue and prefix_sum[i] - prefix_sum[queue[0]] >= k:
                 l = queue.popleft()
                 ans = min(ans, i - l)
-            
-            while queue and prefix_sum[i] <= prefix_sum[queue[-1]]:
+
+            while queue and prefix_sum[queue[-1]] >= prefix_sum[i]:
                 queue.pop()
-            
+
             queue.append(i)
-        
-        return ans if ans != INF else -1
 
-
+        return ans if ans != INF else -1 
 
         
