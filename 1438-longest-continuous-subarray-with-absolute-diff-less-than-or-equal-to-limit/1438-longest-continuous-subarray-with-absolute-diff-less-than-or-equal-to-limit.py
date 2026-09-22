ad1e1daa2caad961648224d988f4b142ac8cb2ac
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-22 20:57:01 +0530

    Time: 206 ms (34.48%), Space: 31.4 MB (47.23%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,36 +1,32 @@
-from collections import deque
-
 class Solution:
-    def longestSubarray(self, nums: List[int], limit: int) -> int:
-        max_queue = deque()
+    def longestSubarray(self, nums: list[int], limit: int) -> int:
+        N = len(nums)
         min_queue = deque()
-        ans = 0
-        n = len(nums)
+        max_queue = deque()
         l = 0
+        ans = 0
 
-        for r in range(n):
-            # remove smaller element from max queue
-            while max_queue and nums[max_queue[-1]] <= nums[r]:
+        for r in range(N):
+            current = nums[r]
+
+            while min_queue and current < nums[min_queue[-1]]:
+                min_queue.pop()
+             
+            while max_queue and current > nums[max_queue[-1]]:
                 max_queue.pop()
+            
+            min_queue.append(r)
             max_queue.append(r)
 
-            # remove maximum element from min queue
-            while min_queue and nums[min_queue[-1]] >= nums[r]:
-                min_queue.pop()
-            min_queue.append(r)
-
-            while nums[max_queue[0]] - nums[min_queue[0]] > limit:
-                if max_queue[0] == l:
-                    max_queue.popleft()
-
+            while max_queue and min_queue and (nums[max_queue[0]] - nums[min_queue[0]]) > limit:
                 if min_queue[0] == l:
                     min_queue.popleft()
                 
+                if max_queue[0] == l:
+                    max_queue.popleft()
+                
                 l += 1
 
-            
             ans = max(ans, r - l + 1)
-        
         return ans
-
-
+        
