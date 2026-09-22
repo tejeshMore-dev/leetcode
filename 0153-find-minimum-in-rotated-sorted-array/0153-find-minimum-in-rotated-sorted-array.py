067d6a348c1928719489a81d42fcbe8acea47513
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-22 23:38:36 +0530

    Time: 0 ms (100%), Space: 19.6 MB (5.58%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -3,9 +3,6 @@ class Solution:
         N = len(nums)
         l = 0
         r = N - 1
-        
-        # if nums[l] < nums[r]:
-        #     return nums[l]
 
         while l < r:
             mid = l + (r - l) // 2
@@ -15,5 +12,5 @@ class Solution:
             else:
                 l = mid + 1
         
-        return nums[r]
+        return nums[l]
         
