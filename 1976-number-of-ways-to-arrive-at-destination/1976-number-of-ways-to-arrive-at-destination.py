1a8f78aa14c116b49e76d40007b701c2059d7914
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-23 20:45:54 +0530

    Time: 5 ms (98.31%), Space: 25.5 MB (38.46%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -1,37 +1,41 @@
 class Solution:
-    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+    def countPaths(self, n: int, roads: list[list[int]]) -> int:
+        graph = [ [] for _ in range(n) ]
+        for u, v, w, in roads:
+            graph[u].append(( v, w ))
+            graph[v].append(( u, w ))
+
         INF = float('inf')
         MOD = 10**9 + 7
 
-        graph = [ [] for _ in range(n) ]
         min_time = [INF] * n
         min_time[0] = 0
         ways = [0] * n
         ways[0] = 1
 
-        for u, v, w in roads:
-            graph[u].append((v, w))
-            graph[v].append((u, w))
-
         min_heap = []
-        heapq.heappush(min_heap, (0, 0))
-        
+        heapq.heappush(min_heap, ( 0, 0 ))
+
         while min_heap:
-            current_time, position = heapq.heappop(min_heap)
-
-            if current_time > min_time[position]:
+            parent_time, parent_node,  = heapq.heappop(min_heap)
+            
+            if parent_time > min_time[parent_node]:
                 continue
-                
-            for nei, time in graph[position]:
-                new_time = current_time + time
+            
+            for nei_node, time in graph[parent_node]:
+                new_time = parent_time + time
 
-                if new_time < min_time[nei]:
-                    min_time[nei] = new_time
-                    heapq.heappush(min_heap, (new_time, nei))
+                if new_time < min_time[nei_node]:
+                    min_time[nei_node] = new_time
+                    ways[nei_node] = ways[parent_node]
 
-                    ways[nei] = ways[position]
-                elif new_time == min_time[nei]:
-                    ways[nei] += ways[position]
-                    ways[nei] %= MOD
-
+                    heapq.heappush(min_heap, ( new_time, nei_node ))
+                elif new_time == min_time[nei_node]:
+                    ways[nei_node] = ( 
+                        ways[nei_node] 
+                        + ways[parent_node]
+                        ) % MOD
+        
         return ways[-1]
+
+        
