3b1fc99a94e7c24d7cb42e50f499312d5fe5d575
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-23 21:10:02 +0530

    Time: 30 ms (81.33%), Space: 32.9 MB (69.01%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,43 +1,47 @@
 class DSU:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.size = [1] * n
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
+    def __init__(self, length):
+        self.length = length
+        self.parent = list(range(self.length))
+        self.size = [1] * self.length
         
+    def find_parent(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find_parent(self.parent[x])
+
         return self.parent[x]
+    
+    def union(self, x, y):
+        parent_x = self.find_parent(x)
+        parent_y = self.find_parent(y)
 
-    def union(self, a, b):
-        root_a = self.find(a)
-        root_b = self.find(b)
-
-        if root_a == root_b:
+        if parent_x == parent_y:
             return False
         
-        if self.size[root_a] < self.size[root_b]:
-            root_a, root_b = root_b, root_a
+        size_x = self.size[x]
+        size_y = self.size[y]
+
+        if size_y > size_x:
+            parent_x, parent_y = parent_y, parent_x
         
-        self.parent[root_b] = root_a
-        self.size[root_a] += self.size[root_b]
+        self.parent[parent_y] = parent_x
+        self.size[parent_x] += size_y
 
         return True
 
 class Solution:
-    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        if len(connections) < n - 1:
+    def makeConnected(self, n: int, connections: list[list[int]]) -> int:
+        edges = len(connections)
+
+        if edges < n - 1:
             return -1
 
         dsu = DSU(n)
-        
-        for a, b in connections:
-            dsu.union(a, b)
-            
-        component = 0
-        for v in range(n):
-            if dsu.find(v) == v:
-                component += 1
+        components = n
 
-        return component - 1
+        for u, v in connections:
+            if dsu.union(u, v):
+                components -= 1
+        
+        return components - 1
+
         
