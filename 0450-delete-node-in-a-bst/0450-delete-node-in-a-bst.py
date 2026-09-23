99f2957e1b09d629a4a953252cda047c32449ed0
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-23 15:07:25 +0530

    Time: 4 ms (12.33%), Space: 22 MB (78.86%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,26 +5,66 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
-            return
-        
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        else:
-            if not root.left:
-                return root.right
-            elif not root.right:
-                return root.left
-            else:
-                succesor = root.right
+    def deleteNode(self, root: TreeNode | None, key: int) -> TreeNode | None:
+        # def delete(node):
+        #     if not node:
+        #         return None
+            
+        #     if not node.left and not node.right:
+        #         return None
 
-                while succesor.left:
-                    succesor = succesor.left
+        #     if node.right:
+        #         succesor = node.right
+
+        #         while succesor and succesor.left:
+        #             succesor.left
                 
-                root.val = succesor.val
-                root.right = self.deleteNode(root.right, succesor.val)
-        
-        return root
+        #         node.val = succesor.val
+
+        #         delete(succesor)
+        #     elif node.left:
+        #         prodecessor = node.left
+
+        #         while prodecessor and prodecessor.right:
+        #             prodecessor.right
+                
+        #         node.val = prodecessor.val
+
+        #         delete(prodecessor)
+        #     else:
+        #         node.val = None
+
+        def helper(node, key):
+            if not node:
+                return None
+            
+            if key == node.val:
+                if not node.left and not node.right:
+                    return None
+
+                elif node.right:
+                    succesor = node.right
+
+                    while succesor and succesor.left:
+                        succesor = succesor.left
+                    
+                    node.val = succesor.val
+                    node.right = helper(node.right, succesor.val)
+
+                elif node.left:
+                    prodecessor = node.left
+
+                    while prodecessor and prodecessor.right:
+                        prodecessor = prodecessor.right
+                    
+                    node.val = prodecessor.val
+                    node.left = helper(node.left, prodecessor.val)
+
+            elif key < node.val:
+                node.left = helper(node.left, key)
+            else:
+                node.right = helper(node.right, key)
+            
+            return node
+
+        return helper(root, key)
