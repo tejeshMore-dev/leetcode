c55d2b8707e0133bfc3a96ae489a0133d5425d3e
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-09-25 10:21:42 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,14 +4,13 @@ class Solution:
         TC : O(n)
         SC : O(1)
         '''
-
         if n < 3:
             return n
 
         one = 2
         two = 1
 
-        for _ in range(n - 3, -1, -1):
+        for _ in range(2, n):
             one, two = one + two, one
 
         return one
